836729bcc736f2445cb1cb098b6d570b696fa47d
Sam Garfinkel <samuel.garfinkel@uconn.edu>
2015-10-20 17:24:39 -0400

    first commit

---
+++ main.py
@@ -0,0 +1,16 @@
+from oauth2client import client, crypt
+from flask import Flask, request, send_file, session
+
+app = Flask(__name__)
+
+@app.route('/login', methods=['GET'])
+def login():
+	return send_file('login.html')
+
+@app.route('/login', methods=['POST'])
+def login_post():
+	token = request.form.get('idtoken')
+	idinfo = client.verify_id_token(token, os.environ['CLIENT_ID']);
+
+if __name__ == '__main__':
+	app.run(debug=True)
