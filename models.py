8b28d3f73b87e181206352dea2f8df9a1d5e5247
Sam Garfinkel <samuel.garfinkel@uconn.edu>
2016-02-22 15:02:20 -0500

    addded models

---
+++ models.py
@@ -0,0 +1,42 @@
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy import Column, Integer, String, ForeignKey
+from sqlalchemy import DateTime, Numeric, func
+from sqlalchemy.dialects.postgresql import UUID
+
+db = SQLAlchemy()
+
+class Categories(db.Model):
+	__tablename__ = 'categories'
+	# Columns
+	id = Column(Integer, primary_key=True)
+	name = Column(String)
+
+	def __repr__(self):
+		return "<Categories(id='%s', name='%s')>" % (self.id, self.name)
+
+class User(db.Model):
+	__tablename__ = 'user'
+	# Columns
+	id = Column(Numeric(scale=32), primary_key=True)
+	email = Column(String)
+	name = Column(String)
+	wid = Column(Integer, primary_key=True)
+
+	def __repr__(self):
+		return "<User(id='%s', email='%s', name='%s', wid='%s')>" % (
+			self.id, self.email, self.name, self.wid)
+
+class Postings(db.Model):
+	__tablename__ = 'postings'
+	# Columns
+	id = Column(Integer, primary_key=True)
+	owner = Column(Numeric(scale=32), ForeignKey('user.id'))
+	description = Column(String)
+	cost = Column(Numeric(precision=2, scale=16))
+	category = Column(Integer, ForeignKey('categories.id'))
+	timestamp = Column(DateTime, default=func.now())
+	title = Column(String)
+
+	def __repr__(self):
+		return "<Postings(id='%s', owner='%s', description='%s', cost='%s', category='%s', timestamp='%s', title='%s')>" % (
+			self.id, self.owner, self.description, self.cost, self.category, self.timestamp, self.title)
