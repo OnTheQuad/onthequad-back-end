89ce0ec06a2617c70af9f2e07ed7276dc7fb57e2
Sam Garfinkel <samuel.garfinkel@uconn.edu>
2016-03-31 17:10:38 -0400

    Another small bug fixed

--- main.py
+++ main.py
@@ -164,7 +164,7 @@ def search():
     query = Postings.query.join(s_ids, Postings.id == s_ids.c.id)
 
     # Return the JSON
-    return jsonify(data=[to_dict(r) for r in page.items], num_pages=page.pages), 200
+    return jsonify(data=[to_dict(r) for r in query.all()], num_pages=page.pages), 200
 
 #### API ####
 # User API:
